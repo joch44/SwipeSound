c48954d2cd45360db6052a4cf153e28a50b9ca46
jcheng2025 <81487855+jcheng2025@users.noreply.github.com>
2025-11-12 00:40:51 -0500

    first

---
+++ SwipeSound/SwipeSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeSoundApp.swift
+//  SwipeSound
+//
+//  Created by Jonathan Cheng on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SwipeSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwipeSoundTests/SwipeSoundTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwipeSoundTests.swift
+//  SwipeSoundTests
+//
+//  Created by Jonathan Cheng on 11/12/25.
+//
+
+import Testing
+@testable import SwipeSound
+
+struct SwipeSoundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
